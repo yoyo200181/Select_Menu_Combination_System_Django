78e0b6559e458635aef6182a2040789ef5bbe406
yoyo200181 <113662537+yoyo200181@users.noreply.github.com>
2025-05-20 15:16:44 +0800

    add User model

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Select_Menu_Combination_System
 
+# add new component
+1. Go to folder /src
+2. Add component: `python3 manage.py startapp component_name`
+3. add Config in settings.py
+
+# superuser ac
+admin
+smcsadmin
---
+++ src/authentication/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError(_('The Login Name must be set'))
+        
+        username = self.normalize_username(username)
+        new_user = self.model(username, **extra_fields)
+        new_user.set_password(password)
+        new_user.save()
+
+        return new_user
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        
+        if extra_fields.get('is_active') is not True:
+            raise ValueError(_('Superuser must have is_active=True.'))
+        
+        return self.create_user(username, password, **extra_fields)
+
+class User(AbstractUser):
+    username = models.CharField(max_length=25, unique=True)
+    phone_number = PhoneNumberField(null=True, unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"<User {self.username}"
